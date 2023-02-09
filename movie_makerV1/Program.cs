df47c9a666554c7a852f42ae44f492f01fdeeca0
jasmi <jasmi@LAPTOP-JKGFQHQL>
2023-02-10 11:39:10 +1300

    Add project files.

---
+++ movie_makerV1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movie_makerV1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
